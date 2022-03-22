c13f1e970bf548bec9081759b273863c066fd446
YRK <yrk199512@gmail.com>
2022-03-22 09:28:22 +0800

    添加权限mock接口

--- src/mock/basis/login.js
+++ src/mock/basis/login.js
@@ -6,6 +6,28 @@ let Login = {
   }
 }
 
+let AccessPermissions = [
+  {
+    path: '/setting',
+    component: () => import('@/layout'),
+    meta: { title: 'setting', icon: 'yiconsetting1' },
+    children: [
+      {
+        path: 'profile',
+        name: 'profile',
+        component: () => import('@/views/system/Profile'),
+        meta: { title: 'profile' }
+      },
+      {
+        path: 'selfSetting',
+        name: 'selfSetting',
+        component: () => import('@/views/system/SettingsPage'),
+        meta: { title: 'selfSetting' }
+      }
+    ]
+  },
+]
+
 export default {
 
   /**
@@ -17,6 +39,23 @@ export default {
     const params = JSON.parse(option['body']);
     if (params['username'] === 'admin' && params['password'] === '123456') {
       return Login
+    }else {
+      return {
+        "code": 500,
+        "success": false,
+        "msg": '账号或者密码错误'
+      }
+    }
+  },
+  /**
+   * 基础 - 登录
+   * @param： {}    
+   * @returns：Promise {<pending>}
+   **/
+  'post|getPermissions': option => {
+    const params = JSON.parse(option['body']);
+    if (params['username'] === 'admin') {
+      return AccessPermissions
     }else {
       return {
         "code": 500,
