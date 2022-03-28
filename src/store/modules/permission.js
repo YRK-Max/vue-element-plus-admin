32ae457ee7304a60d8c7d5b12fc988578f29b78d
YRK <yrk199512@gmail.com>
2022-03-28 19:18:04 +0800

    添加 重定向

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -13,8 +13,11 @@ export const permission = {
     }
   },
   actions: {
-    async generateRoutes(context, roles) {
+    async generateRoutes (context, roles) {
+      // 根据角色从后端获取对应的路由列表
       const routes = await getPermissionsByUser({ roles: roles })
+      // 404 对应路由必须放在路由表的最下面，所以需要在获取动态路由后添加
+      // ‘/:pathMatch(.*)’ vue3中无法直接使用*号
       routes.push({ path: '/:pathMatch(.*)', redirect: '/404', hidden: true });
       handleRoutes(null, routes);
       console.log(router.getRoutes());
@@ -34,15 +37,26 @@ function handleRoutes (parent, routes) {
       meta: r.meta || {}
     };
     let childs = [];
+
+    if(r.redirect) {
+      routeStructer.redirect = r.redirect;
+    }
     
+    // 懒加载中必须使用@/xxx/作为前缀进行拼接，否则会报找不到模块的问题
     if (r.children && r.children.length > 0) {
       r.children.forEach(child => {
-        childs.push({
+        let temp_child = {
           name: child.name || '',
           path: child.path,
           meta: child.meta || {},
           component: () => import(`@/views/${child.component}`)
-        })
+        }
+
+        if(child.redirect) {
+          temp_child.redirect = child.redirect;
+        }
+        
+        childs.push(temp_child);
       })
     }
     routeStructer['children'] = childs;
