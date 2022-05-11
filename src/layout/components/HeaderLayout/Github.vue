b5d9bce573c1174791d8777184c91a0df132b40b
YRK <yrk199512@gmail.com>
2022-05-11 15:22:46 +0800

    优化

--- src/layout/components/HeaderLayout/Github.vue
+++ src/layout/components/HeaderLayout/Github.vue
@@ -7,11 +7,12 @@
       class="flex items-center justify-center h-full px-3 cursor-pointer hover:bg-gray-100"
       @click="handleGoGithub"
     >
-      <i class="yicon-common yicongithub"></i>
+      <YIcon :size="22" icon="yicongithub" />
     </div>
   </el-tooltip>
 </template>
 <script setup>
+import YIcon from '@/components/YIcon.vue'
 function handleGoGithub() {
   window.open('https://github.com/YRK-Max/vue-element-plus-admin')
 }
